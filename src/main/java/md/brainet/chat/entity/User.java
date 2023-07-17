b814276a3a4f6a063928af24fd19b72d6a43e384
Igor Popov <lovespringmvc@gmail.com>
2023-07-17 19:53:14 +0400

    init user class

--- src/main/java/md/brainet/chat/entity/User.java
+++ src/main/java/md/brainet/chat/entity/User.java
@@ -1,5 +1,40 @@
 package md.brainet.chat.entity;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "surname")
+	private String surname;
+	
+	@Column(name = "age")
+	private int age;
+	
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "photo_link")
+	private String photoLink;
+	
+	@Column(name = "bio")
+	private String bio;
 }
