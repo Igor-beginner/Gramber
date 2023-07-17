9ff153a3701b6567adfe07237cf3351aee186f4d
Igor <lovespringmvc@gmail.com>
2023-07-17 20:28:30 +0300

    add spring data jpa in the pom.xml

---
+++ src/main/java/md/brainet/chat/controller/Chat.java
@@ -0,0 +1,14 @@
+package md.brainet.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Chat {
+
+	@GetMapping("/create")
+	public void create() {
+	}
+}
