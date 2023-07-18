fb6f8c4fba6afbb501170e6cfe616c3a28b83088
Igor Popov <lovespringmvc@gmail.com>
2023-07-18 16:15:19 +0400

    add postconstuct and predestroy methods for transactions

--- src/main/java/md/brainet/chat/dao/HibernateChatManagerImpl.java
+++ src/main/java/md/brainet/chat/dao/HibernateChatManagerImpl.java
@@ -1,16 +1,36 @@
 package md.brainet.chat.dao;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import md.brainet.chat.rest.Chat;
 
 @Component
+@RequestScope
 public class HibernateChatManagerImpl implements ChatManager{
 	
 	@Autowired
+	private SessionFactory sessionFactory;
+	
 	private Session session;
+	
+	@PostConstruct
+	public void inti() {
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		session.getTransaction().commit();
+		session.close();
+	}
 	
 	@Override
 	public void addNewChat(Chat newChat) {
@@ -19,7 +39,7 @@ public class HibernateChatManagerImpl implements ChatManager{
 
 	@Override
 	public void deleteChatById(int chatId) {
-		session.createQuery("delete chat where id = :chatId")
+		session.createQuery("delete Chat where id = :chatId")
 		.setParameter("chatId", chatId)
 		.executeUpdate();
 	}
