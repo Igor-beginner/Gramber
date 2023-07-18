ed9c4bc927d6e0fa064eabe0b4c730e92b52484e
Igor Popov <lovespringmvc@gmail.com>
2023-07-18 16:14:59 +0400

    do changes for work with hibernate sessions

--- src/main/java/md/brainet/chat/config/SpringConfiguration.java
+++ src/main/java/md/brainet/chat/config/SpringConfiguration.java
@@ -1,8 +1,13 @@
 package md.brainet.chat.config;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +16,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan("md.brainet.chat")
 public class SpringConfiguration implements WebMvcConfigurer{
 	
+	@Bean
+	public SessionFactory sessionFactory() {
+		return new org.hibernate.cfg.Configuration().configure("hibernate.cgf.xml").buildSessionFactory();
+	}
 }
