46fad71e8d704fdc86bfcc96a15af4562fa204eb
Igor <lovespringmvc@gmail.com>
2023-08-04 11:18:45 +0300

    add messages view and thymleaf config

--- src/main/java/md/brainet/chat/config/SpringConfiguration.java
+++ src/main/java/md/brainet/chat/config/SpringConfiguration.java
@@ -8,16 +8,52 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("md.brainet.chat")
 public class SpringConfiguration implements WebMvcConfigurer{
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/views/");
+	}
+	
 	@Bean
 	public SessionFactory sessionFactory() {
-		return new org.hibernate.cfg.Configuration().configure("hibernate.cgf.xml").buildSessionFactory();
+		return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(springTemplateEngine());
+		return resolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine springTemplateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(iTemplateResolver());
+		return engine;
+	}
+	
+	@Bean
+	public ITemplateResolver iTemplateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver .setPrefix("/views/");
+		resolver .setSuffix("/index.html");
+		resolver .setTemplateMode(TemplateMode.HTML);
+		return resolver ;
 	}
 }
--- src/main/java/md/brainet/chat/controller/Chat.java
+++ src/main/java/md/brainet/chat/controller/Chat.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/messages")
 public class Chat {
 
-	@GetMapping("/create")
-	public void create() {
+	@GetMapping
+	public String getPage() {
+		return "messages";
 	}
 }
